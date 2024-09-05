c3c983e7f931a23bd275173496105b6548f81143
Caterpie <1915036350@qq.com>
2024-09-06 01:52:43 +0800

    优化定时任务的log

--- loop/wired.sh
+++ loop/wired.sh
@@ -7,9 +7,16 @@ T=$3
 echo "[INFO] 定时任务开始"
 
 while true; do
-    echo "[INFO] `date`: 准备执行连接任务"
-    curl -s "http://172.30.255.42:801/eportal/portal/login" -G \
+    echo "[INFO] `date`: 执行连接任务"
+    RESP=(curl -s "http://172.30.255.42:801/eportal/portal/login" -G \
     -d "user_account=$ID" \
-    -d "user_password=$PASSWORD"
+    -d "user_password=$PASSWORD")
+    if echo "$RESP" | grep -q "Portal"; then
+        echo "[INFO] 登陆成功"
+    elif echo "$RESP" | grep -q "IP"; then
+        echo "[INFO] 登陆成功"
+    else
+        echo "[WARN] 登陆失败"
+    fi
     sleep $(($T*60))
 done
--- loop/wlan.sh
+++ loop/wlan.sh
@@ -7,9 +7,14 @@ T=$3
 echo "[INFO] 定时任务开始"
 
 while true; do
-    echo "[INFO] `date`: 准备执行连接任务"
-    curl -s "https://drcom.szu.edu.cn/" -X POST \
+    echo "[INFO] `date`: 执行连接任务"
+    RESP=(curl -s "https://drcom.szu.edu.cn/" -X POST \
     -H "Content-Type:application/x-www-form-urlencoded" \
-    --data-raw "DDDDD=$ID&upass=$PASSWORD&0MKKey=123456"
+    --data-raw "DDDDD=$ID&upass=$PASSWORD&0MKKey=123456")
+    if echo "$RESP" | grep -q "Dr.COMWebLoginID_3.htm"; then
+        echo "[INFO] 登陆成功"
+    else
+        echo "[WARN] 登陆失败"
+    fi
     sleep $(($T*60))
 done
