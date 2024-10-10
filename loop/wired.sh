9e08b34c36db2432cfac43ad905fa66a57406b52
Caterpie <1915036350@qq.com>
2024-10-10 15:43:34 +0800

    添加定时任务日志

--- loop/wired.sh
+++ loop/wired.sh
@@ -4,19 +4,25 @@ ID=$1
 PASSWORD=$2
 T=$3
 
-echo "[INFO] 定时任务开始"
+log() {
+    filename="`dirname $0`/task.log"
+    message=$1
+    echo $message && echo $message >> $filename
+}
+
+log "[INFO] 定时任务开始"
 
 while true; do
-    echo "[INFO] `date`: 执行连接任务"
+    log "[INFO] `date`: 执行连接任务"
     RESP=$(curl -s "http://172.30.255.42:801/eportal/portal/login" -G \
     -d "user_account=$ID" \
     -d "user_password=$PASSWORD")
     if echo "$RESP" | grep -q "Portal"; then
-        echo "[INFO] 登陆成功"
+        log "[INFO] 登陆成功"
     elif echo "$RESP" | grep -q "IP"; then
-        echo "[INFO] 登陆成功"
+        log "[INFO] 登陆成功"
     else
-        echo "[WARN] 登陆失败"
+        log "[WARN] 登陆失败"
     fi
     sleep $(($T*60))
 done
--- loop/wlan.sh
+++ loop/wlan.sh
@@ -4,17 +4,23 @@ ID=$1
 PASSWORD=$2
 T=$3
 
-echo "[INFO] 定时任务开始"
+log() {
+    filename="`dirname $0`/task.log"
+    message=$1
+    echo $message && echo $message >> $filename
+}
+
+log "[INFO] 定时任务开始"
 
 while true; do
-    echo "[INFO] `date`: 执行连接任务"
+    log "[INFO] `date`: 执行连接任务"
     RESP=$(curl -s "https://drcom.szu.edu.cn/" -X POST \
     -H "Content-Type:application/x-www-form-urlencoded" \
     --data-raw "DDDDD=$ID&upass=$PASSWORD&0MKKey=123456")
     if echo "$RESP" | grep -q "Dr.COMWebLoginID_3.htm"; then
-        echo "[INFO] 登陆成功"
+        log "[INFO] 登陆成功"
     else
-        echo "[WARN] 登陆失败"
+        log "[WARN] 登陆失败"
     fi
     sleep $(($T*60))
 done
